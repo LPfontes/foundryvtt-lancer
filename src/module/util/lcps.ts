9a395944d1dd55c8415f4475d43bed50639dc117
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2026-09-16 01:02:38 -0300

    chore: update lancer-data-pt-br to 1.2.3

--- src/module/util/lcps.ts
+++ src/module/util/lcps.ts
@@ -468,7 +468,7 @@ async function massifContentPacks(): Promise<{ id: string; manifest: IContentPac
       manifest: {
         name: "Lancer Core Book (PT-BR)",
         author: "LPfontes / Massif Press",
-        version: "1.2.2",
+        version: "1.2.3",
         website: "https://github.com/LPfontes/lancer-data-pt-br",
       } as IContentPackManifest,
       cpData: (await import("lancer-data-pt-br")) as NpmLancerData,
